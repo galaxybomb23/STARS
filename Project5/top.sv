b45204b0125fe50c1f3958a44b0ebe6765c7b769
galaxybomb23 <jorgenel@purdue.edu>
2023-06-05 15:45:05 -0400

    project 5 version 0.56

--- Project5/top.sv
+++ Project5/top.sv
@@ -16,8 +16,46 @@ module top (
   input  logic txready, rxready
 );
 
-  // your code here...
+  
 
 endmodule
 
-// extra modules here...
+// 4 bit ALU
+module ALU4bit(
+  input logic En, // Enable
+  input logic [2:0] Ctrl, // Control
+  input logic Cin, // Carry in
+  input logic [3:0] A, B, // Inputs
+  output logic [3:0] M, // 4-bit output
+  output logic S, // Sign
+  output logic O, // Overflow
+  output logic Cout // Carry out
+);
+
+always_comb begin
+  if(En) begin
+    case(Ctrl)
+      3'b000: M = A + B + {4{Cin}}; // Addition
+      3'b001: M = A - B + {4{Cin}}; // Subtraction
+      3'b010: M = ~A;               // Bitwise NOT
+      3'b011: M = A & B;            // Bitwise AND
+      3'b100: M = A | B;            // Bitwise OR
+      3'b101: M = A ^ B;            // Bitwise XOR
+      3'b110: M = A << 2;           // A multiplied by 2
+      3'b111: M = A >> 2;           // A divided by 2
+      default: M = 4'bxxxx;
+    endcase
+    S = M[3];
+    O = (A[3] == B[3]) && (A[3] != M[3]);
+    Cout = (Ctrl == 3'b101 && (A[3] & B[3])) || (Ctrl == 3'b110 && (A[3] | B[3]));
+  end
+  else begin
+    M = 4'bxxxx;
+    S = 1'bx;
+    O = 1'bx;
+    Cout = 1'bx;
+  end
+end
+
+
+endmodule
