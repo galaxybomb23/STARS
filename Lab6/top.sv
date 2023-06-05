4a36f088a12308d86a70d354f69c3e442b6b6392
galaxybomb23 <jorgenel@purdue.edu>
2023-06-05 11:47:33 -0400

    remove case

--- Lab6/top.sv
+++ Lab6/top.sv
@@ -62,19 +62,24 @@ module decode3to8(
   input logic [2:0] in,
   output logic [7:0] out
 );
-always_comb
-    case(in)
-    3'b001: out = 8'b00000010;
-    3'b000: out = 8'b00000001;
-    3'b010: out = 8'b00000100;
-    3'b011: out = 8'b00001000;
-    3'b100: out = 8'b00010000;
-    3'b101: out = 8'b00100000;
-    3'b110: out = 8'b01000000;
-    3'b111: out = 8'b10000000;
-    default: out = 8'b00000000;
-  endcase
 
+
+always_comb begin
+  out = 8'b00000000;
+  out[in] = 1'b1;
+
+    // case(in)
+    // 3'b000: out = 8'b00000001;
+    // 3'b001: out = 8'b00000010;
+    // 3'b010: out = 8'b00000100;
+    // 3'b011: out = 8'b00001000;
+    // 3'b100: out = 8'b00010000;
+    // 3'b101: out = 8'b00100000;
+    // 3'b110: out = 8'b01000000;
+    // 3'b111: out = 8'b10000000;
+    // default: out = 8'b00000000;
+    //endcase
+end
 
 endmodule
 
@@ -94,6 +99,7 @@ assign Y =  I[7] ? 3'b111:
             I[0] ? 3'b000:
             3'b000; //nothing
 assign G = |I;
+
 endmodule
 
 module ssdec(
